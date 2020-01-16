76b2d94580b777f280244f0d748b216cce8589d2
James Barciz <jamesjbarciz@gmail.com>
2020-01-15 22:09:48 -0500

    revise setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py - recreated from Unit3-Sprint1-Mod1 class
+
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='dspt3-unit2-freq-used-functions',
+    version='1.0',
+    author='James Barciz',
+    author_email='jamesjbarciz@gmail.com',
+    description='functions for train/val/test-matrix integration and confusion matrix visualization',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/JamesBarciz/unit3-s1-mod1-stretch',
+    keywords='wrangle confusion_matrix',
+    packages=find_packages()
+)
